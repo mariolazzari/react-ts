e3062d449fa40133a8554a805d22a6b4901024b8
Mario Lazzari <mario.lazzari@gmail.com>
2021-07-09 20:15:55 +0200

    Done lesson 16

--- rts/src/index.tsx
+++ rts/src/index.tsx
@@ -1,12 +1,14 @@
 import ReactDOM from "react-dom";
 import GusetList from "./state/GusetList";
 import UserSearch from "./state/UserSearch";
+import EventComponent from "./events/EventComponent";
 
 const App = () => {
   return (
     <div>
       <GusetList />
       <UserSearch />
+      <EventComponent />
     </div>
   );
 };
