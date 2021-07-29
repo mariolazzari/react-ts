ea91ff4ecc764b6e1b14d6d139f16b85f3a8991c
Mario Lazzari <mario.lazzari@gmail.com>
2021-07-29 21:10:45 +0200

    Done lesson 192

--- esb/jbook/src/components/code-cell.tsx
+++ esb/jbook/src/components/code-cell.tsx
@@ -1,13 +1,13 @@
-import { useState, useEffect } from "react";
-import CodeEditor from "./code-editor";
-import Preview from "./preview";
-import bundle from "../bundler";
-import Resizable from "./resizable";
+import { useState, useEffect } from 'react';
+import CodeEditor from './code-editor';
+import Preview from './preview';
+import bundle from '../bundler';
+import Resizable from './resizable';
 
 const CodeCell = () => {
-  const [code, setCode] = useState("");
-  const [err, setErr] = useState("");
-  const [input, setInput] = useState("");
+  const [code, setCode] = useState('');
+  const [err, setErr] = useState('');
+  const [input, setInput] = useState('');
 
   useEffect(() => {
     const timer = setTimeout(async () => {
@@ -23,11 +23,11 @@ const CodeCell = () => {
 
   return (
     <Resizable direction="vertical">
-      <div style={{ height: "100%", display: "flex", flexDirection: "row" }}>
+      <div style={{ height: '100%', display: 'flex', flexDirection: 'row' }}>
         <Resizable direction="horizontal">
           <CodeEditor
             initialValue="const a = 1;"
-            onChange={value => setInput(value)}
+            onChange={(value) => setInput(value)}
           />
         </Resizable>
         <Preview code={code} err={err} />
