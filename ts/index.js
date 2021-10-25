e2542194a5a9d300ec873aac238dc7f74b6b839b
Mario Lazzari <mario.lazzari@gmail.com>
2021-10-25 21:41:32 +0200

    Done lesson 341

--- ts/index.js
+++ ts/index.js
@@ -3,5 +3,5 @@ exports.__esModule = true;
 var axios_1 = require("axios");
 var url = "https://jsonplaceholder.typicode.com/posts/1";
 axios_1["default"].get(url).then(function (response) {
-    console.log(response.data);
+  console.log(response.data);
 });
