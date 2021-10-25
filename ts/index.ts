971c47866487b73df628e383455e76c5de2cd49f
Mario Lazzari <mario.lazzari@gmail.com>
2021-10-25 18:59:35 +0200

    Done lesson 330

--- ts/index.ts
+++ ts/index.ts
@@ -1,7 +1,22 @@
 import axios from "axios";
 
-const url = "https://jsonplaceholder.typicode.com/posts/1";
+const url = "https://jsonplaceholder.typicode.com/todos/1";
+
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
 
 axios.get(url).then(response => {
-  console.log(response.data);
+  const todo = response.data as Todo;
+  logTodo(todo);
 });
+
+const logTodo = (todo: Todo) => {
+  console.log(`
+        ID: ${todo.id}
+        Title: ${todo.title}
+        Finished?: ${todo.completed}
+        `);
+};
