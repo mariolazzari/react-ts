4be222d7ce933dfe072642090268f03b8d06c7ef
Mario Lazzari <mario.lazzari@gmail.com>
2021-10-27 18:29:10 +0200

    Done lesson 352

--- ts/arrays.ts
+++ ts/arrays.ts
@@ -1,4 +1,4 @@
-const carMakers = ["ford", "toyota", "chevy"];
+const carMakers = ['ford', 'toyota', 'chevy'];
 const dates = [new Date(), new Date()];
 
 const carsByMake: string[][] = [];
@@ -11,11 +11,13 @@ const myCar = carMakers.pop();
 carMakers.push(100);
 
 // Help with 'map'
-carMakers.map((car: string): string => {
-  return car.toUpperCase();
-});
+carMakers.map(
+  (car: string): string => {
+    return car.toUpperCase();
+  }
+);
 
 // Flexible types
 const importantDates: (Date | string)[] = [];
-importantDates.push("2030-10-10");
+importantDates.push('2030-10-10');
 importantDates.push(new Date());
